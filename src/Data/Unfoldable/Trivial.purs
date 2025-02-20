ee0fce4117633b289954df7b2dbdfb9e71cdea48
UnrelatedString <unrelatedstring@gmail.com>
2025-02-20 13:58:20 -0500

    Fixed the broken, uh, EVERYTHING that I just added (including a throwaway Functor instance lol)

--- src/Data/Unfoldable/Trivial.purs
+++ src/Data/Unfoldable/Trivial.purs
@@ -15,7 +15,8 @@ import Data.Tuple.Nested ((/\), type (/\))
 import Data.Maybe (Maybe(..))
 import Data.Exists (Exists, mkExists, runExists)
 import Data.Enum (class BoundedEnum, upFromIncluding)
-import Data.Newtype (class Newtype)
+import Data.Newtype (class Newtype, unwrap)
+import Data.Bifunctor (lmap)
 
 -- | A constructor taking the same arguments as `unfoldr`.
 data UnfoldrCall a b = UnfoldrCall (b -> Maybe (a /\ b)) b
@@ -23,7 +24,10 @@ data UnfoldrCall a b = UnfoldrCall (b -> Maybe (a /\ b)) b
 -- | A newtype wrapping `UnfoldrCall a b`, existentially quantified over the "seed" type `b`.
 newtype Trivial a = Trivial (Exists (UnfoldrCall a))
 derive instance Newtype (Trivial a) _
-derive instance Functor (Trivial a)
+
+-- | Internal helper for implementing functions on Trivial.
+untrivial :: forall a c. (forall b. UnfoldrCall a b -> c) -> Trivial a -> c
+untrivial f = runExists f <<< unwrap
 
 -- | Wraps both arguments to `unfoldr` in an `UnfoldrCall`.
 instance trivialUnfoldable :: Unfoldable Trivial where
@@ -32,6 +36,16 @@ instance trivialUnfoldable :: Unfoldable Trivial where
 instance trivialUnfoldable1 :: Unfoldable1 Trivial where
   unfoldr1 = defaultUnfoldr1
 
+instance trivialFunctor :: Functor Trivial where
+  map :: forall a c. (a -> c) -> Trivial a -> Trivial c
+  map f = untrivial eMap
+    where eMap :: forall b. UnfoldrCall a b -> Trivial c
+          eMap (UnfoldrCall g seed) = Trivial
+                                    $ mkExists
+                                    $ UnfoldrCall (
+                                      map (lmap f) <$> g
+                                    ) seed
+
 -- | Provides a default implementation of `unfoldr1` using `unfoldr` to satisfy
 -- | the superclass bound on `Unfoldable`.
 defaultUnfoldr1 :: forall a b t. Unfoldable t => (b -> a /\ Maybe b) -> b -> t a
@@ -39,26 +53,27 @@ defaultUnfoldr1 f = unfoldr (map f) <<< Just
 
 -- | Steps the "generator" forward, possibly returning an element and a new advanced `Trivial`.
 uncons :: forall a. Trivial a -> Maybe (a /\ Trivial a)
-uncons (Trivial e) = runExists eUncons e
+uncons = untrivial eUncons
   where eUncons :: forall b. UnfoldrCall a b -> Maybe (a /\ Trivial a)
         eUncons (UnfoldrCall f seed) = f seed <#> map (unfoldr f)
 
 -- | Converts to any other `Unfoldable`.
 -- | Can also be seen as "evaluating" the inner `UnfoldrCall`.
 runTrivial :: forall a u. Unfoldable u => Trivial a -> u a
-runTrivial (Trivial e) = runExists eRunTrivial
+runTrivial = untrivial eRunTrivial
   where eRunTrivial :: forall b. UnfoldrCall a b -> u a
         eRunTrivial (UnfoldrCall f seed) = unfoldr f seed
 
 instance trivialFoldable :: Foldable Trivial where
   foldl :: forall a c. (c -> a -> c) -> c -> Trivial a -> c
-  foldl f foldInit (Trivial e) = runExists eFoldl e
+  foldl f foldInit = untrivial eFoldl
     where eFoldl :: forall b. UnfoldrCall a b -> c
           eFoldl (UnfoldrCall g unfoldSeed) = lockstep unfoldSeed foldInit
             where lockstep :: b -> c -> c
                   lockstep seed acc
                     | Just (a /\ seed') <- g seed = lockstep seed' $ f acc a
                     | otherwise = acc
+
   foldr f = foldrDefault f
   foldMap f = foldMapDefaultL f
 
