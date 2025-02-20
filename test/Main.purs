32e3d400db59b8615c54799a66447fe63e9436e5
UnrelatedString <unrelatedstring@gmail.com>
2025-02-20 15:14:09 -0500

    Double uncons tests :3

--- test/Main.purs
+++ test/Main.purs
@@ -17,8 +17,9 @@ import Data.Unfoldable.Trivial
   , runTrivial
   , foldEnum
   )
-import Data.Maybe (Maybe(..), isNothing)
-import Data.Tuple (fst)
+import Data.Maybe (Maybe(..), isJust, isNothing)
+import Data.Tuple (fst, snd)
+import Data.Tuple.Nested ((/\), type (/\))
 import Data.Monoid (guard)
 import Data.Unfoldable (none, fromMaybe, replicate)
 import Data.Unfoldable1 (singleton, replicate1)
@@ -35,3 +36,8 @@ smallSuite = suite "small stuff" do
     quickCheck \(x :: Maybe String) -> fst <$> uncons (fromMaybe x) === x
     quickCheck \x -> fst <$> uncons (replicate x "ehehe") === guard (x > 0) (Just "ehehe")
     quickCheck \x (y :: Int) -> fst <$> uncons (replicate1 x y) === Just y
+  test "double uncons" do
+    let double :: forall a. Trivial a -> Maybe (a /\ Maybe a)
+        double = map (map $ map fst <<< uncons) <<< uncons
+    quickCheck \(x :: Int) -> double (singleton x) === Just (x /\ Nothing)
+    quickCheck \x -> isJust (snd =<< double (replicate1 x unit)) === (x > 1)
