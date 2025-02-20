80ce6cb218f279f419685262d380fbe2bb7e2aa0
UnrelatedString <unrelatedstring@gmail.com>
2025-02-20 15:50:07 -0500

    Ohhh. I should... probably go make a Trivial1 for BoundedEnum stuff. come to think of it. thanks testing

--- test/Main.purs
+++ test/Main.purs
@@ -7,6 +7,7 @@ import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Main (runTest)
 import Test.Unit.Assert as Assert
 import Test.QuickCheck ((===))
+import Test.QuickCheck.Arbitrary (class Arbitrary)
 import Test.Unit.QuickCheck (quickCheck)
 
 import Data.Unfoldable.Trivial
@@ -18,15 +19,21 @@ import Data.Unfoldable.Trivial
   , foldEnum
   )
 import Data.Maybe (Maybe(..), isJust, isNothing)
+import Data.Enum (class Enum, class BoundedEnum)
 import Data.Tuple (fst, snd)
 import Data.Tuple.Nested ((/\), type (/\))
 import Data.Monoid (guard)
+import Data.Semigroup.First (First(..))
+import Data.Semigroup.Last (Last(..))
 import Data.Unfoldable (none, fromMaybe, replicate)
 import Data.Unfoldable1 (singleton, replicate1)
+import Data.Newtype (unwrap)
+import Type.Proxy (Proxy(..))
 
 main :: Effect Unit
 main = runTest do
   smallSuite
+  enumSuite
 
 smallSuite :: TestSuite
 smallSuite = suite "small stuff" do
@@ -41,3 +48,21 @@ smallSuite = suite "small stuff" do
         double = map (map $ map fst <<< uncons) <<< uncons
     quickCheck \(x :: Int) -> double (singleton x) === Just (x /\ Nothing)
     quickCheck \x -> isJust (snd =<< double (replicate1 x unit)) === (x > 1)
+  test "Maybe round trip" do
+    quickCheck \(x :: Maybe Char) -> runTrivial (fromMaybe x) === x
+
+enumSuite :: TestSuite
+enumSuite = suite "enums" do
+  genericBoundedEnumSuite "Char" (Proxy :: Proxy Char) $ pure unit
+
+genericEnumSuite :: forall a. Enum a => Arbitrary a => Show a =>
+  String -> Proxy a -> TestSuite -> TestSuite
+genericEnumSuite name _ extras = suite name do
+  extras
+
+genericBoundedEnumSuite :: forall a. BoundedEnum a => Arbitrary a => Show a =>
+  String -> Proxy a -> TestSuite -> TestSuite
+genericBoundedEnumSuite name p extras = genericEnumSuite name p $ (_ <> extras) do
+  test "everything" do
+    Assert.equal (Just $ First bottom) $ foldEnum $ Just <<< (First :: a -> First a)
+    Assert.equal (Just $ Last top) $ foldEnum $ Just <<< (Last :: a -> Last a)
