e12fa9496065de9fb0bc0f73774a78034e16de4b
UnrelatedString <unrelatedstring@gmail.com>
2025-03-06 16:18:05 -0500

    aaaand I have to wait for the Registry to actually update with the transfer...

--- src/Data/Unfoldable/Newtype.purs
+++
@@ -1,9 +0,0 @@
-module Data.Unfoldable.Newtype
- ( NothingIfNone(..)
- ) where
-
-import Data.Newtype (class Newtype)
-import Data.Maybe (Maybe)
-
-newtype NothingIfNone f a = NothingIfNone (Maybe (f a))
-derive instance Newtype (NothingIfNone f a) _
