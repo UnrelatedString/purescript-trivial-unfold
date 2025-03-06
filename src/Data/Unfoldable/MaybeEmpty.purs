982aa9ac108f049af80650fb43c9ef76241b2fdc
UnrelatedString <unrelatedstring@gmail.com>
2025-03-06 17:44:09 -0500

    and this like. doesn't really need to use Trivial at all since it is its own type BUT it does still save me from basically rewriting cons

--- src/Data/Unfoldable/MaybeEmpty.purs
+++ src/Data/Unfoldable/MaybeEmpty.purs
@@ -2,10 +2,30 @@ module Data.Unfoldable.MaybeEmpty
  ( MaybeEmpty(..)
  ) where
 
-import Data.Newtype (class Newtype)
-import Data.Maybe (Maybe)
+import Prelude
 
+import Data.Newtype (class Newtype)
+import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\))
+import Data.Unfoldable
+ ( class Unfoldable1
+ , class Unfoldable
+ , unfoldr1
+ , unfoldr
+ )
+import Data.Unfoldable.Trivial (cons)
+
+-- | Lift an `Unfoldable1` into an `Unfoldable` by wrapping it in `Maybe`,
+-- | such that an empty sequence gives `Nothing` and a nonempty sequence gives
+-- | a nonempty `Unfoldable1` in `Just`.
 newtype MaybeEmpty :: forall k. (k -> Type) -> k -> Type
 newtype MaybeEmpty f a = MaybeEmpty (Maybe (f a))
 derive instance Newtype (MaybeEmpty f a) _
 
+instance maybeEmptyUnfoldable1 :: Unfoldable1 f => Unfoldable1 (MaybeEmpty f) where
+  unfoldr1 = (<<<) (MaybeEmpty <<< Just) <<< unfoldr1
+
+instance maybeEmptyUnfoldable :: Unfoldable1 f => Unfoldable (MaybeEmpty f) where
+  unfoldr f seed
+    | Just (a /\ seed') <- f seed = MaybeEmpty $ Just $ cons a $ unfoldr f seed'
+    | otherwise = MaybeEmpty Nothing
