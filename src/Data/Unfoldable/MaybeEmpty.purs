7b56b88f13875b82ec2949881d4c49f03164d35f
UnrelatedString <unrelatedstring@gmail.com>
2025-03-07 12:26:34 -0500

    added a bunch of other instances. except Eq1 and Ord1???

--- src/Data/Unfoldable/MaybeEmpty.purs
+++ src/Data/Unfoldable/MaybeEmpty.purs
@@ -4,10 +4,17 @@ module Data.Unfoldable.MaybeEmpty
 
 import Prelude
 
-import Data.Newtype (class Newtype, over)
+import Data.Newtype (class Newtype, over, unwrap)
 import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
 import Control.Apply (lift2)
+import Data.Functor.Invariant (class Invariant, imapF)
+import Data.Generic.Rep (class Generic)
+import Data.Eq (class Eq1)
+import Data.Ord (class Ord1)
+import Data.Traversable (class Traversable, traverse)
+import Test.QuickCheck.Arbitrary (class Arbitrary, class Coarbitrary)
+import Control.Alternative (class Alt, class Plus, class Alternative)
 import Data.Unfoldable
  ( class Unfoldable1
  , class Unfoldable
@@ -21,7 +28,20 @@ import Data.Unfoldable.Trivial (cons)
 -- | a nonempty `Unfoldable1` in `Just`.
 newtype MaybeEmpty :: forall k. (k -> Type) -> k -> Type
 newtype MaybeEmpty f a = MaybeEmpty (Maybe (f a))
+
 derive instance Newtype (MaybeEmpty f a) _
+derive newtype instance Show (f a) => Show (MaybeEmpty f a)
+derive newtype instance Eq (f a) => Eq (MaybeEmpty f a)
+derive newtype instance Ord (f a) => Ord (MaybeEmpty f a)
+derive newtype instance Arbitrary (f a) => Arbitrary (MaybeEmpty f a)
+derive newtype instance Coarbitrary (f a) => Coarbitrary (MaybeEmpty f a)
+derive newtype instance Semigroup (f a) => Semigroup (MaybeEmpty f a)
+derive newtype instance Semigroup (f a) => Monoid (MaybeEmpty f a)
+derive newtype instance Bounded (f a) => Bounded (MaybeEmpty f a)
+derive newtype instance Semiring (f a) => Semiring (MaybeEmpty f a)
+derive instance Generic (MaybeEmpty f a) _
+-- derive instance Eq1 f => Eq1 (MaybeEmpty f)
+-- derive instance Ord1 f => Ord1 (MaybeEmpty f)
 
 instance maybeEmptyUnfoldable1 :: Unfoldable1 f => Unfoldable1 (MaybeEmpty f) where
   unfoldr1 = (<<<) (MaybeEmpty <<< Just) <<< unfoldr1
@@ -39,3 +59,11 @@ instance maybeEmptyApply :: Apply f => Apply (MaybeEmpty f) where
 
 instance maybeEmptyApplicative :: Applicative f => Applicative (MaybeEmpty f) where
   pure = MaybeEmpty <<< pure <<< pure
+
+instance maybeEmptyBind :: (Bind f, Traversable f) => Bind (MaybeEmpty f) where
+  bind (MaybeEmpty a) f = MaybeEmpty $ bind a $ map join <<< traverse (unwrap <<< f)
+
+instance maybeEmptyMonad :: (Applicative f, Bind f, Traversable f) => Monad (MaybeEmpty f)
+
+instance maybeEmptyInvariant :: Functor f => Invariant (MaybeEmpty f) where
+  imap = imapF
